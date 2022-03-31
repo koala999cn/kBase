26532355cbe2cf5400b70f4b418acaed279f1ab2
koala999 <koala999@aliyun.com>
2022-03-31 19:55:12 +0800

    init commit

---
+++ src/KuThread.cpp
@@ -0,0 +1,8 @@
+﻿#include "KuThread.h"
+#include <thread>
+
+
+void KuThread::sleep(float msec)
+{
+	std::this_thread::sleep_for(std::chrono::duration<float, std::milli>(msec));
+}
---
+++ src/KuThread.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+// 多线程的使用函数
+
+class KuThread
+{
+public:
+	static void sleep(float msec);
+
+private:
+	KuThread() {}
+	~KuThread() {}
+};
